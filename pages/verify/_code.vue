2ef01883da1af2c3888810a02e610ddd3ee89b6d
fullsirkool <adamaanvaan@gmail.com>
2023-07-25 17:09:38 +0700

    Add new page

---
+++ pages/verify/_code.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="text-cyan-700">{{ title }}</h1>
+</template>
+<script setup>
+const title = ref("Your account is verified!");
+const route = useRoute();
+const code = route.params.code;
+const verifyUser = async () => {
+  const url = `${BASE_URL}auth/verify/${code}`;
+  try {
+    const res = useFetch(url, {
+      method: "PATCH",
+    });
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+};
+verifyUser();
+</script>
+<style></style>
